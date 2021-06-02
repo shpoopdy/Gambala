c622ada6ea4b474cc8e103cb660c08ba32893045
Mikey <shpoopdy@gmail.com>
2021-06-02 14:45:54 -0700

    Add blackJackRules files.

---
+++ blackJackRules.cpp
@@ -0,0 +1,20 @@
+/* cpp file for function: diceRules
+
+By: Mikey
+Last modified: 2021-06-02
+ */
+#include <iostream>
+#include <string>
+#include "blackJackRules.h"
+
+using namespace std;
+
+void blackJackRules()
+{
+    system("cls");
+    cout << "========== Blackjack Game Rules ==========" << endl;
+    cout << "1. Get close to 21 without going over." << endl;
+    cout << "2. You're playing against the computer!" << endl;
+    cout << "3. Place bet and hit when you want a card or stay." << endl;
+    getchar(); //To give the player time to read the rules.
+}
---
+++ blackJackRules.h
@@ -0,0 +1,15 @@
+/* Header file for function: blackJackRules
+
+By: Mikey
+Last modified: 2021-06-02
+ */
+
+#ifndef BLACKJACKRULES_H
+#define BLACKJACKRULES_H
+
+#include <iostream>
+#include <string>
+
+void blackJackRules();
+
+#endif
