2e68fa5f580013d4ae07bd56cb108ac3d46a77ae
Mikey <shpoopdy@gmail.com>
2021-06-07 14:24:59 -0700

    Add dealer vector and logic

--- games/cards/blackJack/blackJackGame.cpp
+++ games/cards/blackJack/blackJackGame.cpp
@@ -24,6 +24,7 @@ void blackJackGame()
     bool playing = true;
     int balance;
     vector<Cards> player;
+    vector<Cards> dealer;
     ifstream in_stream;
     in_stream.open("balance.txt");
     in_stream >> balance;
@@ -52,15 +53,13 @@ void blackJackGame()
       srand (time (0));
       int randomOne = rand() % 52;
       int randomTwo = rand() % 52;
+      int randomThree = rand() % 52;
+      int hit;
+      bool bust = false;
+      bool dealerBust = false;
 
 
 
-      player.push_back(card_ptr[randomOne]);
-      player.push_back(card_ptr[randomTwo]);
-
-      cout << player[0].get_numb() << endl;
-      cout << player[1].get_numb() << endl;
-      getchar();
 
       do
       {
@@ -72,9 +71,81 @@ void blackJackGame()
         }
       }while(bettingAmount > balance);
 
+
+      int playerTotal = 0;
+      int dealerTotal = 0;
+      player.push_back(card_ptr[randomOne]);
+      player.push_back(card_ptr[randomTwo]);
+      dealer.push_back(card_ptr[randomThree]);
+      cout << "You have " << player[0].get_numb() << " and ";
+      cout << player[1].get_numb() << endl;
+      playerTotal = player[0].get_numb() + player[1].get_numb();
+
+      cout << "Dealer has " << dealer[0].get_numb() << "." << endl;
+      dealerTotal += dealer[0].get_numb();
+      cout << "Dealer total " << dealerTotal << "." << endl;
+
+
+
+
+      do {
+        int loopRand = rand() % 52;
+        cout << "Your total is " << playerTotal << "." << endl;
+        cout << "Press 1 for another card or 2 to stay: ";
+        cin >> hit;
+        if(hit == 1)
+        {
+          cout << "You received a " << card_ptr[loopRand].get_numb() << "!\n";
+          player.push_back(card_ptr[loopRand]);
+          playerTotal += card_ptr[loopRand].get_numb();
+          if(playerTotal > 21)
+          {
+            cout << "Oof, looks like you bust son." << endl;
+            bust = true;
+          }
+        }
+        else if(hit == 2)
+        {
+          do {
+            int dealLoop = rand() % 52;
+            cout << "Dealer received a " << card_ptr[dealLoop].get_numb() << "!\n";
+            dealer.push_back(card_ptr[dealLoop]);
+            dealerTotal += card_ptr[dealLoop].get_numb();
+            cout << "Dealer total is " << dealerTotal << "." << endl;
+            
+
+            if(dealerTotal > 21)
+            {
+              cout << "Dealer has bust!" << endl;
+              dealerBust = true;
+            }
+          }while(dealerTotal < 17 && dealerBust == false);
+        }
+      }while(hit == 1 && bust == false);
+
+      if(bust == true)
+      {
+        cout << "Sorry, but you bust. You lost $" << bettingAmount << ".\n";
+      }
+      else if(dealerBust == true)
+      {
+        cout << "Dealer bust! You've won $" << bettingAmount * 3 << ".\n";
+      }
+      else if(playerTotal > dealerTotal)
+      {
+        cout << "You won with " << playerTotal << "! "
+             << "You've earned $" << bettingAmount * 3 << ".\n";
+      }
+      else
+      {
+        cout << "House wins! Sorry, but you've lost $" << bettingAmount << ".\n";
+      }
+
+
       cout << "Want to continue?";
       cin >> choice;
       player.clear();
+      dealer.clear();
 
       if((choice == 'y') || (choice == 'Y'))
       {
