15e0195c5b517fe9ff58863ad4e8f57d9126fde7
Mikey <shpoopdy@gmail.com>
2021-07-06 14:24:13 -0700

    Add comments and reworked grammar

--- games/cards/blackJack/blackJackGame.cpp
+++ games/cards/blackJack/blackJackGame.cpp
@@ -17,7 +17,6 @@ Last modified: 2021-06-18
 
 void blackJackGame()
 {
-    int AMOUNT = 52;
     Card *card_ptr = NULL;
     int bettingAmount;
     char choice;
@@ -52,6 +51,7 @@ void blackJackGame()
 
       do
       {
+        //Check to make sure they have enough funds.
         std::cout << "Enter your bet ";
         std::cin >> bettingAmount;
         if(bettingAmount > balance)
@@ -65,6 +65,7 @@ void blackJackGame()
       int dealerTotal = 0;
       for(int i = 0; i < 2; i++)
       {
+        //Deals two cards to the player.
         card_ptr = deck.TopCard();
         player.add(*card_ptr);
         delete card_ptr;
@@ -73,34 +74,36 @@ void blackJackGame()
       }
     
       std::cout << "You have "; player.display_hand();
-      playerTotal = player.getTotal();
-      std::cout << " Your total is " << playerTotal << ". ";
 
       card_ptr = deck.TopCard();
       dealer.add(*card_ptr);
-      delete card_ptr;
       deck.PopCard();
-      card_ptr = NULL;
+      
 
-      std::cout << "Dealer has a "; dealer.display_hand();
-      dealerTotal = dealer.getTotal();
+      std::cout << " Dealer has a "; card_ptr->test();
+      std::cout << ".\n";
+      delete card_ptr;
+      card_ptr = NULL;
+      //dealerTotal = dealer.getTotal();
       
 
 
 
 
       do {
-        std::cout << " Your total is " << playerTotal << ".\n";
+        std::cout << "Your total is " << player.getTotal() << ".\n";
         std::cout << "Press 1 for another card or 2 to stay: ";
         std::cin >> hit;
         if(hit == 1)
         {
           card_ptr = deck.TopCard();
-          std::cout << "You received a " << card_ptr->getValue() << "!\n";
+          std::cout << "You received a ";
+          card_ptr->test();
+          std::cout << std::endl;
           player.add(*card_ptr);
-          playerTotal = player.getTotal();
+          //playerTotal = player.getTotal();
           deck.PopCard();
-          if(playerTotal > 21)
+          if(player.getTotal() > 21)
           {
             std::cout << "Oof, looks like you bust son.\n";
             playerBust = true;
@@ -112,21 +115,23 @@ void blackJackGame()
         {
           do {
             card_ptr = deck.TopCard();
-            std::cout << "Dealer received a " << card_ptr->getValue() << "!\n";
+            std::cout << "Dealer received a ";
+            card_ptr->test();
             dealer.add(*card_ptr);
-            dealerTotal = dealer.getTotal();
+            //dealerTotal = dealer.getTotal();
             deck.PopCard();
+            std::cout << std::endl;
             std::cout << "Dealer total is " << dealer.getTotal() << ".\n";
             
 
-            if(dealerTotal > 21)
+            if(dealer.getTotal() > 21)
             {
               std::cout << "Dealer has bust!\n";
               dealerBust = true;
             }
             delete card_ptr;
             card_ptr = NULL;
-          }while(dealerTotal < 17 && dealerBust == false);
+          }while(dealer.getTotal() < 17 && dealerBust == false);
         }
       }while(hit == 1 && playerBust == false);
 
@@ -140,13 +145,13 @@ void blackJackGame()
         std::cout << "Dealer bust! You've won $" << bettingAmount * 2 << ".\n";
         balance = balance + (bettingAmount * 2);
       }
-      else if(playerTotal > dealerTotal)
+      else if(player.getTotal() > dealer.getTotal())
       {
         std::cout << "You won with " << player.getTotal() << "! "
              << "You've earned $" << bettingAmount * 2 << ".\n";
         balance = balance + (bettingAmount * 2);
       }
-      else if(playerTotal == dealerTotal)
+      else if(player.getTotal() == dealer.getTotal())
       {
         std::cout << "It's a push! You get your money back.";
       }
@@ -162,7 +167,12 @@ void blackJackGame()
       player.clear();
       dealer.clear();
 
-      if((choice == 'y') || (choice == 'Y'))
+      if(balance == 0)
+      {
+        std::cout << "Looks like you've reached your end..." << std::endl;
+        playing = false;
+      }
+      else if((choice == 'y') || (choice == 'Y'))
       {
         playing = true;
       }
