7d6deb2171a1a0c56009de2e14b9eef6d5ad2605
Mikey <shpoopdy@gmail.com>
2021-10-04 10:58:02 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include "Gambala.cpp"
+#include "games/dice/diceRules.h"
+#include "games/dice/diceGame.h"
+#include "games/cards/blackJack/blackJackGame.h"
+#include "games/cards/blackJack/Card.h"
+#include "games/cards/blackJack/Deck.h"
+using namespace std;
+
+
+int main() {
+    Gambala();
+
+
+
+    return EXIT_SUCCESS;
+}
