852466c25837ea3b00d7e0fa5614e2d42a5a05a8
Mikey <shpoopdy@gmail.com>
2021-10-10 20:44:55 -0700

    Remove <<endl;

--- games/dice/diceRules.cpp
+++ games/dice/diceRules.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 void diceRules() {
     system("cls");
-    cout << "========== Dice Game Rules ==========" << endl;
-    cout << "1. Choose a number from 1 to 10." << endl;
-    cout << "2. If ya win, you get 10 times what ya bet!" << endl;
-    cout << "3. If ya lose though, well that's just how it goes." << endl;
+    cout << "========== Dice Game Rules ==========\n";
+    cout << "1. Choose a number from 1 to 10.\n";
+    cout << "2. If ya win, you get 10 times what ya bet!\n";
+    cout << "3. If ya lose though, well that's just how it goes.\n";
     getchar(); //To give the player time to read the rules.
 }
